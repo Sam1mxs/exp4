637adb7db4a7ea5195932b0bcfe2b4e212fecad2
LxFee <gravitylmlml@gmail.com>
2021-11-16 22:09:41 +0800

    修改main

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
     BigNumber a(1231), b("32999"), c;
-    c = a * b;
+    c = a + b;
     cout << a.to_string() << endl;
     cout << b.to_string() << endl;
     cout << c.to_string() << endl;
